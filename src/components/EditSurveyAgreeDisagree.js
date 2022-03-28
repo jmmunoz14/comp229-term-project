bb2bacea8adf121ba59f92ac19cbb56d7a1e1791
Cristopher Moreno Palmer <cmorenop@my.centennialcollege.ca>
2022-03-28 19:01:46 -0400

    Edit Surveys

--- src/components/EditSurveyAgreeDisagree.js
+++ src/components/EditSurveyAgreeDisagree.js
@@ -1,10 +1,46 @@
+import React, { useState, useRef, useEffect } from 'react'
+
+
 const EditSurveyAgreeDisagree = ({ surveyTitle, id }) => {
 
+
+    //set max quzntity to  10 question
+
+    const questionRef = useRef()
+
+    //Hooks => useState  
+    const [question, setQuestion] = useState([])
+    const [counter, setCounter] = useState(0)
+
+    useEffect(() => {
+  
+    }, [question])
+
+
+//when click on add append to the array
+    function renderNewQuestion() {
+
+        
+    }
+
+
     return (
+
         <div>
-            {surveyTitle}
-            {id}
+            <button onClick={renderNewQuestion}>Add</button>
+
+            <p>Question </p>
+            <label><input type="text"  /></label>
+
+
+
+
+            <div>
+                <button>submit</button>
+            </div>
         </div>
+
+
     );
 }
 
