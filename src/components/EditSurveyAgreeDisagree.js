d7386fe2f3f5d1f8452da342edf32b43781f7d99
Cristopher Moreno Palmer <cmorenop@my.centennialcollege.ca>
2022-03-29 08:28:40 -0400

    Add questions [agree-disagree]

--- src/components/EditSurveyAgreeDisagree.js
+++ src/components/EditSurveyAgreeDisagree.js
@@ -1,47 +1,97 @@
 import React, { useState, useRef, useEffect } from 'react'
 
 
-const EditSurveyAgreeDisagree = ({ surveyTitle, id }) => {
+export default function EditSurveyAgreeDisagree({ surveyTitle, id }) {
 
 
     //set max quzntity to  10 question
 
-    const questionRef = useRef()
-
     //Hooks => useState  
-    const [question, setQuestion] = useState([])
-    const [counter, setCounter] = useState(0)
+    const [question, setQuestion] = useState([]);
+    const [questionCount, setQuestionCount] = useState(0);
+    const [currentQuestion, setCurrentQuestion] = useState(0);
 
     useEffect(() => {
-  
-    }, [question])
+        setQuestionCount(questionCount);
+    }, [questionCount])
 
+    // helper function
+    let createQuestionSet = (containerId, numOfQuestions) => {
+        for (let i = 0; i<numOfQuestions; i++) {
+            let questionPrompt = document.createElement("p");
+            let questionField = document.createElement("input");
+            let questionSaveButton = document.createElement("button");
+            let nodeText = document.createTextNode(`Question ${i+1}`);
 
-//when click on add append to the array
-    function renderNewQuestion() {
+            questionPrompt.setAttribute("id", `prompt${i}`);
+            questionField.setAttribute("id", `field${i}`);
+            questionSaveButton.setAttribute("id", `saveBtn${i}`);
 
-        
+            questionSaveButton.onclick = function() {
+                let newQuestion = {
+                    questionId: i,
+                    question: questionField.value,
+                }
+                setQuestion(arr => [...arr, newQuestion]);
+                alert(`Successfully added Question ${i+1} to Survey`)
+            }
+
+            questionPrompt.appendChild(nodeText);
+            questionSaveButton.appendChild(document.createTextNode("Save Question"));
+
+            containerId.appendChild(questionPrompt);
+            containerId.appendChild(questionField);
+            containerId.appendChild(questionSaveButton)
+        }
     }
 
+    //when click on add append to the array
+    function renderQuestionSet() {
+        let numOfQuestions = parseInt(document.getElementById("numOfQuestions").value);
+        let questionContainer = document.getElementById("questionContainer");
+
+        if (numOfQuestions <= 0) {
+            alert("Please enter at least 1 (or more) questions");
+        } else if (numOfQuestions > 10) { 
+            alert("You have inputted too many questions. Please enter a maximum of 10 questions")
+        } else {
+            // refer to helper function above
+            createQuestionSet(questionContainer, numOfQuestions);
+        }
+
+        setQuestionCount(() => numOfQuestions);
+    }
 
     return (
 
-        <div>
-            <button onClick={renderNewQuestion}>Add</button>
+        <div className='container'>
+            <h1>{surveyTitle}</h1>
+            <p>{id}</p>
+            <p>Input # of Questions</p>
+            <label><input id="numOfQuestions" type="text"  /></label>            
+            <button className='btn' onClick={renderQuestionSet}>Add</button>
 
-            <p>Question </p>
-            <label><input type="text"  /></label>
-
-
-
+            <div id="questionContainer">
+            </div>
 
             <div>
-                <button>submit</button>
+                Question Count: {questionCount}
+            </div>
+            <div className="" id="currentQuestionSet">
+                List of Questions: 
+                {question.map((q, index) => {
+                    return (
+                        <div key={index}>
+                        Question {q.questionId+1}: {q.question}
+                        </div>
+                    )
+                })}
+            </div>
+            <div>
+                <button className='btn'>submit</button>
             </div>
         </div>
 
 
     );
 }
-
-export default EditSurveyAgreeDisagree;
