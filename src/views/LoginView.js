e4d6b17c483368ca1d9b8c21ca1fb343d2981d1e
jmmunoz14 <jm.munoz14@uniandes.edu.co>
2022-04-16 15:58:25 -0500

    Update LoginView.js

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,73 +1,82 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import Auth from '../components/Auth'
-import axios from 'axios';
-
+import axios from 'axios'
 
 export const LoginView = () => {
+  const emptyForm = {
+    username: '',
+    password: '',
+  }
 
+  const [form, setForm] = useState(emptyForm)
+  const [loadingUser, setLoadingUser] = useState(true)
 
-    const emptyForm = {
-        username: '',
-        password: '',
-    }
+  const handleChange = (evt) => {
+    const value = evt.target.value
+    setForm({
+      ...form,
+      [evt.target.name]: value,
+    })
+    console.log(form)
+  }
 
-    const [form, setForm] = useState(emptyForm);
-    const [loadingUser, setLoadingUser] = useState(true);
+  const handleSubmit = (event) => {
+    event.preventDefault()
 
-    const handleChange = (evt) => {
+    axios
+      .post('https://surveymeanbackend.herokuapp.com/users/signin', form)
+      .then((response) => {
+        if (response.data.success == true) {
+          //alert(response.data.token)
+          //alert("Success")
+          localStorage.setItem('token', response.data.token)
+          window.location.replace('/')
+        }
+      })
+      .catch((error) => {
+        alert('Error: ' + error.message)
+      })
+  }
 
-        const value = evt.target.value;
-        setForm({
-            ...form,
-            [evt.target.name]: value
-        });
-        console.log(form)
-    }
-
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-
-        axios.post('http://surveymeanbackend.herokuapp.com/users/signin', form)
-            .then(response => {
-
-                if (response.data.success == true) {
-                    //alert(response.data.token)
-                    //alert("Success")
-                    localStorage.setItem('token', response.data.token);
-                    window.location.replace("/");
-                }
-            })
-            .catch(error => {
-                alert("Error: " + error.message)
-            })
-    }
-
-    return (
-        <div className='container'>
-            <form onSubmit={handleSubmit}>
-                <div className='register-btn-flexbox-container'>
-                    <table className='center'>
-                        <tbody>
-                            <tr>
-                                <label>Username </label>
-                                <input type="text" name="username" className="form-control"
-                                    onChange={handleChange}
-                                    required />
-                            </tr>
-                            <tr>
-                                <label>Password</label>
-                                <input type="password" name="password" className="form-control"
-                                    onChange={handleChange}
-                                    required />
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div className='register-btn-flexbox-container'> <button type='submit' className="btn">Login</button></div>
-                <div className='register-btn-flexbox-container'><a href="/signup"> Sign-up </a></div>
-            </form>
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="register-btn-flexbox-container">
+          <table className="center">
+            <tbody>
+              <tr>
+                <label>Username </label>
+                <input
+                  type="text"
+                  name="username"
+                  className="form-control"
+                  onChange={handleChange}
+                  required
+                />
+              </tr>
+              <tr>
+                <label>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  className="form-control"
+                  onChange={handleChange}
+                  required
+                />
+              </tr>
+            </tbody>
+          </table>
         </div>
-    )
+        <div className="register-btn-flexbox-container">
+          {' '}
+          <button type="submit" className="btn">
+            Login
+          </button>
+        </div>
+        <div className="register-btn-flexbox-container">
+          <a href="/signup"> Sign-up </a>
+        </div>
+      </form>
+    </div>
+  )
 }
-
