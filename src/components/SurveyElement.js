e489991559fafccaa08a0c6640f0a3378e75af18
Cristopher Moreno Palmer <cmorenop@my.centennialcollege.ca>
2022-04-15 21:22:15 -0400

    delete endpoint

--- src/components/SurveyElement.js
+++ src/components/SurveyElement.js
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios';
 
 
 const SurveyElement = ({ surveyTitle, id, surveyType }) => {
@@ -6,7 +7,25 @@ const SurveyElement = ({ surveyTitle, id, surveyType }) => {
     let takeSurvey = () => { navigate('/surveys/' + id) }
     let editSurvey = () => { navigate('/editSurvey', { state: { surveyTitle, id, surveyType } }) }
     let addQuestions = () => { navigate('/addQuestions', { state: { surveyTitle, id, surveyType } }) }
-    let previewQuestions = () => { navigate('/surveys/preview/'+ id, { state: { surveyTitle, id, surveyType } }) }
+    let previewQuestions = () => { navigate('/surveys/preview/' + id, { state: { surveyTitle, id, surveyType } }) }
+
+    function deleteSurvey() {
+        const token = localStorage.getItem('token')
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+
+        const body = {}
+
+        axios.delete(`http://surveymeanbackend.herokuapp.com/survey/delete/${id}`, body, {
+            headers: headers
+        }).then(res => {
+            alert("ok")
+        }).catch(error => {
+            alert(error)
+        })
+    }
 
     return (
         <div key={id}>
@@ -16,7 +35,7 @@ const SurveyElement = ({ surveyTitle, id, surveyType }) => {
                         <td><button className="btn" onClick={takeSurvey}> Take </button></td>
                         <td><button className="btn" onClick={editSurvey}>Edit</button></td>
                         <td><button className="btn-add" onClick={addQuestions}>Add</button></td>
-                        <td style={{ justifyContent: 'flex-end' }}><button className="btn-delete">Delete</button></td>
+                        <td style={{ justifyContent: 'flex-end' }}><button className="btn-delete" onClick={deleteSurvey}>Delete</button></td>
                         <td style={{ justifyContent: 'flex-end' }} onClick={previewQuestions} ><button className="btn-preview">Preview</button></td>
 
                         {surveyType.agreeDisagree &&
